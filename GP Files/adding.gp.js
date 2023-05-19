98e64263daf3bb9ea996f336cee0e9e03d988a57
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-05-19 22:34:25 +0100

    Create adding.gp.js

---
+++ GP Files/adding.gp.js
@@ -0,0 +1,11 @@
+func st() rt:int[0]{
+  def var num1 = 22;
+  def var num2 = 230;
+  def var num3 = 5347;
+  
+  op("The sum of 22, 230, and 5347 is: {!num1 + num2 + num3}");
+}
+
+
+// Outpu:
+// The sum of 22, 230, and 5347 is: 5599
