2e504b60c7eb0bfb88e636d3648b4a3d2af1075a
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-05-19 22:31:28 +0100

    Update nicknames.gp.js

--- GP Files/nicknames.gp.js
+++ GP Files/nicknames.gp.js
@@ -1,5 +1,5 @@
 func st() rt:int[0]{
-  def var reallyLongVar:<LongVar> = "This is an extremely long and verbose variable. It is this to demonstrate how nicknames in GreetLang+ work. Thank you for wasting time reading this long, verbose, and extremely boilerplate variable value, which has the type of a string. Have a nice day!";
+  def var reallyLongVar:<LongVar> = "This is an extremely long and verbose variable. It is this to demonstrate how nicknames in GreetLang+ work. Thank you for wasting time reading this long, excessively verbose, and extremely boilerplate variable value, which has the type of a string. Have a nice day!";
   
   op("{n:LongVar}");
 }
@@ -7,4 +7,4 @@ func st() rt:int[0]{
 
 
 // Output:
-// This is an extremely long and verbose variable. It is this to demonstrate how nicknames in GreetLang+ work. Thank you for wasting time reading this long, verbose, and extremely boilerplate variable value, which has the type of a string. Have a nice day!
+// This is an extremely long and verbose variable. It is this to demonstrate how nicknames in GreetLang+ work. Thank you for wasting time reading this long, excessively verbose, and extremely boilerplate variable value, which has the type of a string. Have a nice day!
