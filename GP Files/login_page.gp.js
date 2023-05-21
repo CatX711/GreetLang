c07c3bc7e416184086f8bbe2f0e0f070e88b3bd4
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-05-21 21:10:38 +0100

    Update login_page.gp.js

--- GP Files/login_page.gp.js
+++ GP Files/login_page.gp.js
@@ -1,35 +1,34 @@
 import gpultim.gph
 
 func st() rt:int[0]{
-  
 	
-def var tries = 0;
+  def var tries = 0;
   
-op("Booting...");
+  op("Booting...");
 
-while tries < 6 {
-  def var user = readline.op("Hello, welcome to PIE OS. What is your username? ");
+  while tries < 6 {
+    def var user = readline.op("Hello, welcome to PIE OS. What is your username? ");
 
-  // if username is correct
-  if user == "pr0gramme5" {
-    def var pass = readline.op("Please enter your password to continue. ");
+    // if username is correct
+    if user == "pr0gramme5" {
+      def var pass = readline.op("Please enter your password to continue. ");
 
       
-    // if password is correct
-    if pass = "sharksurfer23"{ 
-       op("Welcome, {!user}");
-       end; // ends while loop
-    }
-       // if password is incorrect
-    else {
-      op("Incorrect, try again.");
-      tries += 1;
+      // if password is correct
+      if pass = "sharksurfer23"{ 
+         op("Welcome, {!user}");
+         end; // ends while loop
+      }
+      // if password is incorrect
+      else {
+        op("Incorrect, try again.");
+        tries += 1;
+       }
      }
-   }
     
-    // if username is wrong 
-   else {
-     op("Incorrect, try again.");
-     tries += 1;
-   }
+     // if username is wrong 
+     else {
+       op("Incorrect, try again.");
+       tries += 1;
+     }
 }
